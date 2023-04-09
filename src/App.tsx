2dd0e5ad73fe88bab253fee026d95c95c3ed934e
Jorge Luis Rojas Arcos <jorge.rojas@benandfrank.com>
2023-04-09 00:12:55 -0600

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
 import { useState } from 'react'
-import viteLogo from '../../../../../vite.svg'
 import './App.css'
 import reactLogo from './assets/react.svg'
 
-function App () {
+function App (): JSX.Element {
   const [count, setCount] = useState(0)
 
   return (
     <div className='App'>
       <div>
-        <a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
-        </a>
         <a href='https://reactjs.org' target='_blank' rel='noreferrer'>
           <img src={reactLogo} className='logo react' alt='React logo' />
         </a>
