481c6e2de4f24cba4ae63bb825e5d29776c70273
Jorge Rojas <jorge.rojas@benandfrank.com>
2023-04-09 21:05:59 -0600

    feat: add tailwind setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import '@global-styles/main.css'
 import { Firebase } from './components'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById('root') as HTMLElement).render(
+  <StrictMode>
     <Firebase />
-    <App />
-  </React.StrictMode>
+    <h1 className='text-xl font-extrabold'>Everyone has the right to an effective remedy by the competent national tribunals for acts violating the fundamental rights granted him by the constitution or by law.</h1>
+  </StrictMode>
 )
